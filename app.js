44ab2d9eaeaeb10a140c60a606aa63b6542e9b55
Owes Ansari <Owes.Ansari@teknopoint.local>
2023-02-09 17:55:31 +0530

    git remote add origin https://github.com/avais1021/Notes-Taking-App.git git branch -M main git push -u origin main

---
+++ app.js
@@ -0,0 +1,67 @@
+
+const add_noteBtn = document.querySelector("#add_noteBtn");
+const main = document.querySelector(".main");
+
+add_noteBtn.addEventListener("click", function(){
+    addNote();
+});
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".noteBox textarea")
+    // console.log(notes)
+    const data = [];
+    notes.forEach(function(notesitem){
+        // console.log(notesitem.value);
+        data.push(notesitem.value)
+    })
+    // console.log(data);
+    if(data.length === 0){
+        localStorage.removeItem("notes")
+    } else {
+        localStorage.setItem("notes" , JSON.stringify(data));
+    }
+}
+
+
+const addNote = (text = "") => {
+    const noteBox = document.createElement("div");
+    noteBox.setAttribute("class", "noteBox ");
+    noteBox.innerHTML = `
+    <div class="tool">
+    <i class=" save fa-solid fa-floppy-disk"></i>
+    <i class=" trash fa-solid fa-trash"></i>
+    </div>
+    <textarea>${text}</textarea>
+    `;
+    main.appendChild(noteBox);
+    noteBox.querySelector(".trash").addEventListener("click" , function(){
+        noteBox.remove();
+        saveNotes();
+    })
+    noteBox.querySelector(".save").addEventListener("click" , function(){
+        saveNotes();
+    })
+    noteBox.querySelector("textarea").addEventListener("keyup" , function(){
+        saveNotes();
+    })
+    saveNotes();
+}
+
+
+(
+    function(){
+        const lsNotes =JSON.parse( localStorage.getItem("notes") );
+        console.log(lsNotes)
+        if(lsNotes === null) {
+            addNote();
+        } else {           
+            lsNotes.forEach((lsNotes) => {
+                addNote(lsNotes);
+            })
+        }
+       
+    }
+)()
+
+
+
